0e47022a4a53d59e4c4d0b7191db9921fa28f344
Jacquelyn Bradshaw <jacquelyn_bradshaw@live.co.uk>
2023-04-24 20:18:55 +0100

    session store created

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 const path = require("path")
 
 const express = require("express")
+const session = require("express-session")
+const csrf = require("csurf")
 
+const sessionConfig = require("./config/session")
 const db = require("./data/database")
+const authRoutes = require("./routes/auth")
+const addCSRFTokenMiddleware = require("./middlewares/csrf-token-middleware")
+
+const MongoDBSessionStore = sessionConfig.createSessionStore(session)
 
 const app = express()
 
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 
-app.use(express.urlencoded({extended: false}))
 app.use(express.static("public"))
+app.use(express.urlencoded({extended: false}))
+
+app.use(session(sessionConfig.createSessionConfig(MongoDBSessionStore)))
+
+app.use(csrf())
+
+app.use(addCSRFTokenMiddleware)
 
 app.get("/", function(req, res) {
   res.render("index")
 })
+
+app.use(authRoutes)
 
 db.connectToDatabase().then(function () {
   app.listen(3000)
