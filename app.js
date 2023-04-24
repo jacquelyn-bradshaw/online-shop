a849ec39f8a2e908bb33518d5385f8a0e992611d
Jacquelyn Bradshaw <jacquelyn_bradshaw@live.co.uk>
2023-04-24 19:40:59 +0100

    database set up

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const path = require('path')
+const path = require("path")
 
 const express = require("express")
 
+const db = require("./data/database")
+
 const app = express()
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
 
 app.use(express.urlencoded({extended: false}))
-app.use(express.static('public'))
+app.use(express.static("public"))
 
 app.get("/", function(req, res) {
   res.render("index")
 })
 
-app.listen(3000)
+db.connectToDatabase().then(function () {
+  app.listen(3000)
+})
