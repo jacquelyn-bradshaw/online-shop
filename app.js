94865267efdfbfad03a4780cef691c215f750de8
Jacquelyn Bradshaw <jacquelyn_bradshaw@live.co.uk>
2023-05-26 19:58:28 +0100

    styling to log out button in header fixed

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(productRoutes)
 
 app.use(function(error, req, res, next) {
   console.log(JSON.stringify(error));
-  res.render('errors/500');
+  res.status(500).render('errors/500');
 })
 
 db.connectToDatabase()
